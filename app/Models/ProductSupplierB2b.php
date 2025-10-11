af17ef071aa66d817f4f856dc0d78a1332a98191
mysupplyme <admin@mysupply.me>
2025-10-11 08:34:03 +0300

    changes to product controller

--- app/Models/ProductSupplierB2b.php
+++ app/Models/ProductSupplierB2b.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class ProductSupplierB2b extends Model
 {
-    protected $table = 'product_suppliers__b2b';
+    protected $table = 'product_suppliers_b2b';
     protected $fillable = ['product_id', 'supplier_id', 'title', 'description', 'price', 'date', 'location'];
     
     public function category()
